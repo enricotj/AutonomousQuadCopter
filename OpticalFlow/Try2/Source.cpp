f212fec45dc92b24fd776a4e01ebc43db4773cbd
Austin May <mayja1@rose-hulman.edu>
2015-11-02 20:13:33 -0500

    Trying to do something else with tracking

--- OpticalFlow/Try2/Source.cpp
+++ OpticalFlow/Try2/Source.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
-#include <raspicam/raspicam_cv.h>
+//#include <raspicam/raspicam_cv.h>
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 
-#include <raspicam/raspicam.h>
+//#include <raspicam/raspicam.h>
 
 #include "WiringPi-master/wiringPi/wiringPi.h"
 #include "WiringPi-master/wiringPi/softServo.h"
+#include <opencv2/core/utility.hpp>
+#include "opencv2/video/tracking.hpp"
+#include "opencv2/videoio.hpp"
 
+#include <ctype.h>
+
+using namespace cv;
+using namespace std;
+
+Mat image;
+
+bool backprojMode = false;
+bool selectObject = false;
+int trackObject = 0;
+bool showHist = true;
+Point origin;
+Rect selection;
+int vmin = 10, vmax = 256, smin = 30;
 
 using namespace cv;
 using namespace std;
@@ -17,21 +34,21 @@ void moveServo(int rot)
 	switch (rot)
 	{
 		// move left
-		case -1:
-			softServoWrite(0, 0);
-			break;
+	case -1:
+		softServoWrite(0, 0);
+		break;
 		// stop
-		case 0:
-			softServoWrite(0, 400);
-			break;
+	case 0:
+		softServoWrite(0, 400);
+		break;
 		// move right
-		case 1:
-			softServoWrite(0, 1000);
-			break;
+	case 1:
+		softServoWrite(0, 1000);
+		break;
 		// stop
-		default:
-			softServoWrite(0, 400);
-			break;
+	default:
+		softServoWrite(0, 400);
+		break;
 	}
 }
 
@@ -39,223 +56,117 @@ int lowX;
 int highX;
 int lowY;
 int highY;
-bool track;
-int num;
-Mat imgOriginal;
-Mat trackMatrix;
-int xIndex;
-int yIndex;
-
 vector<Mat> frames;
-
-int px = -1;
-int py = -1;
-
-
-
-void SaveState(int state, void*) {
-	trackMatrix = imgOriginal.adjustROI(highY, lowY, lowX, highX);
-	track = true;
-}
-
-void findObject() {
-	float min = -1;
-	int xTrackIndex = 0;
-	int yTrackIndex = 0;
-	int range = 5; //(trackMatrix.rows + trackMatrix.cols) / 4;
-	int rows = trackMatrix.rows;
-	int cols = trackMatrix.cols;
-	int yDiff = rows/50;
-	int xDiff = cols/50;
-	for (int a = yIndex - range; a < yIndex + range; a++) {
-		/*if ((a + rows) >= highY){
-			break;
-		}*/
-		for (int b = xIndex - range; b < xIndex + range; b++) {
-			if (a < 0){
-				a = 0;
-			}if (b < 0){
-				b = 0;
-			}
-			/*if ((b +cols) >= highX) {
-				break;
-			}*/
-			float diff = 0;
-			//printf("x: %d, y: %d, d:%f\n", b, a, min);
-			bool ex = false;
-			for (int y = 0; y < rows; y+=yDiff)
-			{
-				for (int x = 0; x < cols; x+=xDiff)
-				{
-					try {
-						Vec3b imgColor = imgOriginal.at<Vec3b>(Point(b + x, a + y));
-						Vec3b color = trackMatrix.at<Vec3b>(Point(x, y));
-						diff += abs(imgColor[0] - color[0])*4 + abs(imgColor[1] - color[1]) + abs(imgColor[2] - color[2]);
-					}
-					catch (Exception e) {
-						ex = true;
-					}
-				}
-			}
-			if ((min == -1 || diff < min) && !ex) {
-				min = diff;
-				xTrackIndex = b;
-				yTrackIndex = a;
-			}
-		}
-		
-	}
-	
-	if(xTrackIndex <= xIndex - range + 1) {
-		moveServo(1);
-		cout<<"move right"<<endl;
-	} else if (xTrackIndex < xIndex + range - 1) {
-		moveServo(0);
-		cout<<"stop"<<endl;	
-	}else{
-		moveServo(-1);
-		cout<<"move left"<<endl;	
-	}
-
-	yIndex = yTrackIndex;
-}
 int main(int argc, char** argv)
 {
 	wiringPiSetup();
 	softServoSetup(0, 1, 2, 3, 4, 5, 6, 7);
-/*
-	for (int i = 0; i<25; i++)
-	{
-		moveServo(-1);
-	}
 
-	moveServo(1);
-	moveServo(0);
-	moveServo(-1);
-
-	while(true)
-	{
-		moveServo(0);
-	}
-
-	delay(5000);
-*/
-	//system("sudo modprobe bcm2835-v4l2");
-	//system("sudo modprobe v4l2-common");
-	//system("v4l2-ctl --overlay=1");
 	raspicam::RaspiCam_Cv Camera; //Cmaera object
 	// Open Camera
-	Camera.set(CV_CAP_PROP_FORMAT, CV_8UC3);   
-Camera.set(CV_CAP_PROP_FRAME_WIDTH, 640);
-Camera.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+	Camera.set(CV_CAP_PROP_FORMAT, CV_8UC3);
+	Camera.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+	Camera.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
 	if (!Camera.open())
 	{
 		cout << "Cannot open the web cam" << endl;
 		return -1;
 	}
-
-	/*VideoCapture cap(0); //capture the video from web cam
-	if (!cap.isOpened())  // if not success, exit program
-	{
-	cout << "Cannot open the web cam" << endl;
-	return -1;
-	}
-
-	*/
 	lowX = 319 - 32;
 	highX = 319 + 32;
 	lowY = 239 - 32;
 	highY = 239 + 32;
-	track = false;
-	num = 1;
-	int i = 0;
-	while(true){
-		i++;
-		if(i>13000000) break;
-	}
+	selection.x = MIN(highX, lowX);
+	selection.y = MIN(highY, lowY);
+	selection.width = std::abs(highX - lowX);
+	selection.height = std::abs(highY - lowY);
 
-	VideoWriter video("out.avi",CV_FOURCC('M','J','P','G'),24,Size(640,480),true);
+	selection &= Rect(0, 0, image.cols, image.rows);
+	Rect trackWindow;
+	int hsize = 16;
+	float hranges[] = { 0, 180 };
+	const float* phranges = hranges;
+	int i = 0;
+	while (true){
+		i++;
+		if (i > 13000000) break;
+	}
+	Mat frame, hsv, hue, mask, hist, histimg = Mat::zeros(200, 320, CV_8UC3), backproj;
+	VideoWriter video("out.avi", CV_FOURCC('M', 'J', 'P', 'G'), 24, Size(640, 480), true);
 
 	//namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
 	//namedWindow("Rectangle Image", CV_WINDOW_KEEPRATIO);
-	
+
 	//Create trackbars in "Control" window
-	bool first = false;
-	i =0;
-	while (i<100)
+	i = 0;
+	while (i < 100)
 	{
 		/*bool bSuccess = cap.read(imgOriginal); // read a new frame from video
 		if (!bSuccess) //if not success, break loop
 		{
-			cout << "Cannot read a frame from video stream" << endl;
-			break;
+		cout << "Cannot read a frame from video stream" << endl;
+		break;
 		}
 		*/
-		
+
 		Camera.grab();
-		Camera.retrieve(imgOriginal);
+		Camera.retrieve(image);
+		cvtColor(image, hsv, COLOR_BGR2HSV);
 
-		/*
-		if (!first) {
-			cvCreateTrackbar("Low X", "Control", &lowX, imgOriginal.cols); //Hue (0 - 179)
-			cvCreateTrackbar("High x", "Control", &highX, imgOriginal.cols);
-			highX = imgOriginal.cols;
-			cvCreateTrackbar("Low Y", "Control", &lowY, imgOriginal.rows); //Saturation (0 - 255)
-			cvCreateTrackbar("High Y", "Control", &highY, imgOriginal.rows);
-			cvCreateTrackbar("Hack", "Control", &num, 1);
-			highY = imgOriginal.rows;
-			first = true;
-			//cvCreateButton("Track", SaveState, NULL, CV_PUSH_BUTTON, 1);
-			//createButton("Track", SaveState, NULL, 0, false);
-		}
-		*/
-		if (num == 1 && !track) {
-			Mat imgHSV;
-			imgOriginal.copyTo(imgHSV);
-			trackMatrix = imgHSV(Rect(lowX, lowY, highX - lowX, highY - lowY));
-			xIndex = lowX;
-			yIndex = lowY;
-			px = lowX;
-			py = lowY;
-			track =true;
-		}
-		//cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
-		
+		int _vmin = vmin, _vmax = vmax;
 
-		//imshow("Original", imgOriginal); //show the original image
-		
-		Point low = Point(lowX, lowY);
-		Point high = Point(highX, highY);
-		if (track) {
-			findObject();
-			int dx = xIndex - px;
-			int dy = yIndex - py;
-			if (sqrt(pow(dx, 2) + pow(dy, 2)) > 1)
+		inRange(hsv, Scalar(0, smin, MIN(_vmin, _vmax)),
+			Scalar(180, 256, MAX(_vmin, _vmax)), mask);
+		int ch[] = { 0, 0 };
+		hue.create(hsv.size(), hsv.depth());
+		mixChannels(&hsv, 1, &hue, 1, ch, 1);
+
+		if (trackObject < 0)
+		{
+			Mat roi(hue, selection), maskroi(mask, selection);
+			calcHist(&roi, 1, 0, maskroi, hist, 1, &hsize, &phranges);
+			normalize(hist, hist, 0, 255, NORM_MINMAX);
+
+			trackWindow = selection;
+			trackObject = 1;
+
+			histimg = Scalar::all(0);
+			int binW = histimg.cols / hsize;
+			Mat buf(1, hsize, CV_8UC3);
+			for (int i = 0; i < hsize; i++)
+				buf.at<Vec3b>(i) = Vec3b(saturate_cast<uchar>(i*180. / hsize), 255, 255);
+			cvtColor(buf, buf, COLOR_HSV2BGR);
+
+			for (int i = 0; i < hsize; i++)
 			{
-				xIndex += (xIndex - px) / 2;
-				yIndex += (yIndex - py) / 2;
+				int val = saturate_cast<int>(hist.at<float>(i)*histimg.rows / 255);
+				rectangle(histimg, Point(i*binW, histimg.rows),
+					Point((i + 1)*binW, histimg.rows - val),
+					Scalar(buf.at<Vec3b>(i)), -1, 8);
 			}
-			low = Point(xIndex, yIndex);
-			high = Point(xIndex + (highX - lowX), yIndex + (highY - lowY));
-			px = xIndex;
-			py = yIndex;
 		}
-		Mat rectImg;
-		imgOriginal.copyTo(rectImg);
-		cv::rectangle(rectImg, low, high, Scalar(0, 0, 255), 1, 8, 0);
-		//rectImg.convertTo(rectImg, CV_8UC3, 255.0);
-		frames.push_back(rectImg);
-		//imwrite("raspicam_cv_image.jpg", rectImg);
-		//video.write(rectImg);
-		//imshow("Rectangle Image", rectImg); //show the thresholded image
-	
+
+		calcBackProject(&hue, 1, 0, hist, backproj, &phranges);
+		backproj &= mask;
+		RotatedRect trackBox = CamShift(backproj, trackWindow,
+			TermCriteria(TermCriteria::EPS | TermCriteria::COUNT, 10, 1));
+		if (trackWindow.area() <= 1)
+		{
+			int cols = backproj.cols, rows = backproj.rows, r = (MIN(cols, rows) + 5) / 6;
+			trackWindow = Rect(trackWindow.x - r, trackWindow.y - r,
+				trackWindow.x + r, trackWindow.y + r) &
+				Rect(0, 0, cols, rows);
+		}
+
+		if (backprojMode)
+			cvtColor(backproj, image, COLOR_GRAY2BGR);
+		ellipse(image, trackBox, Scalar(0, 0, 255), 3, LINE_AA);
+		//cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
 
 
-
-
-
+		frames.push_back(image);
 		i++;
-		cout <<"loop"<<endl;
+		cout << "loop" << endl;
 	}
 
 	for (vector<Mat>::iterator it = frames.begin(); it != frames.end(); ++it)
@@ -263,7 +174,7 @@ Camera.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
 		video.write(*it);
 	}
 
-	Camera.release();
+	//Camera.release();
 	return 0;
 
 }
