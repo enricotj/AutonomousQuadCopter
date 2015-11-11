9cd50853c2aef4628dc1edde15fef843bae998bd
Animignis <animignis@gmail.com>
2015-11-10 22:11:14 -0500

    Fixed MeanShiftTracker Initial Selection

--- OpticalFlow/Try2/MeanShiftTracker.cpp
+++ OpticalFlow/Try2/MeanShiftTracker.cpp
@@ -16,11 +16,8 @@ int vmin = 10, vmax = 256, smin = 30;
 int hsize = 16;
 float* phranges;
 
-bool paused = true;
-
 static const int SELECTION_EVENT_A = 0;
 static const int SELECTION_EVENT_B = 1;
-int selectState = SELECTION_EVENT_A;
 
 MeanShiftTracker::MeanShiftTracker()
 {
@@ -30,6 +27,9 @@ MeanShiftTracker::MeanShiftTracker(Rect window)
 {
 	histimg = Mat::zeros(200, 320, CV_8UC3);
 	trackWindow = window;
+
+	initSelection(SELECTION_EVENT_A, trackWindow.x, trackWindow.y);
+	initSelection(SELECTION_EVENT_B, trackWindow.x + trackWindow.width, trackWindow.y + trackWindow.height);
 }
 
 MeanShiftTracker::~MeanShiftTracker()
@@ -51,8 +51,6 @@ void MeanShiftTracker::initSelection(int event, int x, int y)
 		selection.y = MIN(y, origin.y);
 		selection.width = std::abs(x - origin.x);
 		selection.height = std::abs(y - origin.y);
-
-		selection &= Rect(0, 0, image.cols, image.rows);
 	}
 
 	switch (event)
@@ -75,77 +73,62 @@ Mat MeanShiftTracker::process(Mat frame)
 	float hranges[] = { 0, 180 };
 	const float* phranges = hranges;
 	frame.copyTo(image);
-	if (!paused)
+	cvtColor(image, hsv, COLOR_BGR2HSV);
+
+	if (trackObject)
 	{
-		cvtColor(image, hsv, COLOR_BGR2HSV);
+		int _vmin = vmin, _vmax = vmax;
 
-		if (trackObject)
+		inRange(hsv, Scalar(0, smin, MIN(_vmin, _vmax)),
+			Scalar(180, 256, MAX(_vmin, _vmax)), mask);
+		int ch[] = { 0, 0 };
+		hue.create(hsv.size(), hsv.depth());
+		mixChannels(&hsv, 1, &hue, 1, ch, 1);
+
+		if (trackObject < 0)
 		{
-			int _vmin = vmin, _vmax = vmax;
+			Mat roi(hue, selection);
+			Mat maskroi(mask, selection);
+			calcHist(&roi, 1, 0, maskroi, hist, 1, &hsize, &phranges);
+			normalize(hist, hist, 0, 255, NORM_MINMAX);
 
-			inRange(hsv, Scalar(0, smin, MIN(_vmin, _vmax)),
-				Scalar(180, 256, MAX(_vmin, _vmax)), mask);
-			int ch[] = { 0, 0 };
-			hue.create(hsv.size(), hsv.depth());
-			mixChannels(&hsv, 1, &hue, 1, ch, 1);
+			trackWindow = selection;
+			trackObject = 1;
 
-			if (trackObject < 0)
+			histimg = Scalar::all(0);
+			int binW = histimg.cols / hsize;
+			Mat buf(1, hsize, CV_8UC3);
+			for (int i = 0; i < hsize; i++)
+				buf.at<Vec3b>(i) = Vec3b(saturate_cast<uchar>(i*180. / hsize), 255, 255);
+			cvtColor(buf, buf, COLOR_HSV2BGR);
+
+			for (int i = 0; i < hsize; i++)
 			{
-				Mat roi(hue, selection);
-				Mat maskroi(mask, selection);
-				calcHist(&roi, 1, 0, maskroi, hist, 1, &hsize, &phranges);
-				normalize(hist, hist, 0, 255, NORM_MINMAX);
-
-				trackWindow = selection;
-				trackObject = 1;
-
-				histimg = Scalar::all(0);
-				int binW = histimg.cols / hsize;
-				Mat buf(1, hsize, CV_8UC3);
-				for (int i = 0; i < hsize; i++)
-					buf.at<Vec3b>(i) = Vec3b(saturate_cast<uchar>(i*180. / hsize), 255, 255);
-				cvtColor(buf, buf, COLOR_HSV2BGR);
-
-				for (int i = 0; i < hsize; i++)
-				{
-					int val = saturate_cast<int>(hist.at<float>(i)*histimg.rows / 255);
-					rectangle(histimg, Point(i*binW, histimg.rows),
-						Point((i + 1)*binW, histimg.rows - val),
-						Scalar(buf.at<Vec3b>(i)), -1, 8);
-				}
+				int val = saturate_cast<int>(hist.at<float>(i)*histimg.rows / 255);
+				rectangle(histimg, Point(i*binW, histimg.rows),
+					Point((i + 1)*binW, histimg.rows - val),
+					Scalar(buf.at<Vec3b>(i)), -1, 8);
 			}
-
-			calcBackProject(&hue, 1, 0, hist, backproj, &phranges);
-			backproj &= mask;
-			trackBox = CamShift(backproj, trackWindow,
-				TermCriteria(TermCriteria::EPS | TermCriteria::COUNT, 10, 1));
-			if (trackWindow.area() <= 1)
-			{
-				int cols = backproj.cols, rows = backproj.rows, r = (MIN(cols, rows) + 5) / 6;
-				trackWindow = Rect(trackWindow.x - r, trackWindow.y - r,
-					trackWindow.x + r, trackWindow.y + r) &
-					Rect(0, 0, cols, rows);
-			}
-
-			if (backprojMode)
-				cvtColor(backproj, image, COLOR_GRAY2BGR);
-			ellipse(image, trackBox, Scalar(0, 0, 255), 3, LINE_AA);
 		}
+
+		calcBackProject(&hue, 1, 0, hist, backproj, &phranges);
+		backproj &= mask;
+		trackBox = CamShift(backproj, trackWindow,
+			TermCriteria(TermCriteria::EPS | TermCriteria::COUNT, 10, 1));
+		if (trackWindow.area() <= 1)
+		{
+			int cols = backproj.cols, rows = backproj.rows, r = (MIN(cols, rows) + 5) / 6;
+			trackWindow = Rect(trackWindow.x - r, trackWindow.y - r,
+				trackWindow.x + r, trackWindow.y + r) &
+				Rect(0, 0, cols, rows);
+		}
+
+		if (backprojMode)
+			cvtColor(backproj, image, COLOR_GRAY2BGR);
+		ellipse(image, trackBox, Scalar(0, 0, 255), 3, LINE_AA);
 	}
-	if (trackObject < 0)
-		paused = false;
 
 	//imshow("Track", image);
-
-	if (selectState < 2)
-	{
-		//waitKey(10);
-		if (selectState == 0)
-			initSelection(selectState, trackWindow.x, trackWindow.y);
-		if (selectState == 1)
-			initSelection(selectState, trackWindow.x + trackWindow.width, trackWindow.y + trackWindow.height);
-		selectState++;
-	}
 
 	return image;
 }
